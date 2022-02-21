2b1b55adf6d8c1f36b627a817dfedd03d505987e
Ricardo271 <ric.zanluchi@gmail.com>
2022-02-21 11:38:07 -0300

    Switching player each turn

--- src/application/App.java
+++ src/application/App.java
@@ -19,7 +19,7 @@ public class App {
 
             try {
 
-                UI.printBoard(chessMatch.getPieces());
+                UI.printMatch(chessMatch);
                 System.out.println();
                 System.out.print("Source: ");
                 ChessPosition source = UI.readChessPosition(sc);
